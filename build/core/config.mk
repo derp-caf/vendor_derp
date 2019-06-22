b352a14e27ef19a33f8ee49bda6195687d14b30e
Ethan Chen <intervigil@gmail.com>
2019-06-22 13:30:38 -0700

    build: Reorganize inclusion of QCOM macros
    * There's actually not a whole lot QC-specific about these macros,
      make them available at definition.mk inclusion time rather than
      relying on BoardConfig to include them.
    * Don't create a dependency on QCOM macros from a makefile
      that is included very early in the build process.
    
    Change-Id: I3dcaf0e068ca44732cfe58f69384621781ddda67

--- build/core/config.mk
+++ build/core/config.mk
@@ -5,3 +5,8 @@ include $(TOPDIR)vendor/derp/build/core/qcom_target.mk
 ifneq ($(TARGET_BUILD_VARIANT),user)
 SELINUX_IGNORE_NEVERALLOWS := true
 endif
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/derp/build/core/qcom_target.mk
+include $(TOPDIR)vendor/derp/build/core/qcom_utils.mk
+>>>>>>> fdc1d40... build: Reorganize inclusion of QCOM macros
