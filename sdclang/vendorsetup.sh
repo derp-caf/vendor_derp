aryaman895/android_vendor_reloaded

vendor: Update sdclang configuration
Change-Id: I33ba5c12f083c8b75cd7ba9c2c67ae4ab3101e4d

sdclang/vendorsetup.sh
@@ -1,20 +1,4 @@
-# Set SDClang defaults
-export SDCLANG=false
-export SDCLANG_PATH=vendor/qcom/sdclang-6.0/linux-x86/bin
-export SDCLANG_LTO_DEFS=vendor/reloaded/sdclang/sdllvm-lto-defs.mk
-export SDCLANG_COMMON_FLAGS="-O3 -fvectorize -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
--Wno-deprecated-register -Wno-tautological-type-limit-compare -Wno-sign-compare -Wno-gnu-folding-constant \
--mllvm -arm-implicit-it=always -Wno-inline-asm -Wno-unused-command-line-argument -Wno-unused-variable"
-
-# Enable based on host OS/availablitiy
-case $(uname -s) in
-    Linux)
-        if [ -d "$SDCLANG_PATH" ]; then
-            export SDCLANG=true
-        fi
-        ;;
-    Darwin)
-        ;;
-    *)
-        ;;
-esac
+# SDClang Environment Variables
+export SDCLANG_AE_CONFIG=$(pwd)/vendor/reloaded/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=$(pwd)/vendor/reloaded/sdclang/sdclang.json
+export SDCLANG_SA_ENABLED=false

derp-caf/vendor_derp

sdclang/vendorsetup.sh
@@ -1,0 +1,4 @@
+# SDClang Environment Variables
+export SDCLANG_AE_CONFIG=$(pwd)/vendor/derp/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=$(pwd)/vendor/derp/sdclang/sdclang.json
+export SDCLANG_SA_ENABLED=false