821e0ad40dac3ce3b0c789d203c82f3823d1dd1e
ZyxxeZ <bankersenator@gmail.com>
2020-01-25 12:01:51 +0000

    vendor: more cleanup for Q

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,7 +1,19 @@
 # Telephony packages
 PRODUCT_PACKAGES += \
+    messaging \
     Stk \
     CellBroadcastReceiver
+
+# RCS
+PRODUCT_PACKAGES += \
+    rcscommon \
+    rcscommon.xml \
+    rcsservice \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_aidl_static \
+    rcs_service_api \
+    rcs_service_api.xml
 
 # IMS
 PRODUCT_PACKAGES += \
